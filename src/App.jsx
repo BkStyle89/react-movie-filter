341607dfd899b6d63cb9078a0047d4592efa4bd8
BkStyle89 <kajzer@hotmail.it>
2026-03-16 11:42:33 +0100

    boostrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
 
+[
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
+
 function App() {
+
+const [films,setFilms] = useState('')
 
   return (
     <>
-      
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          
+        </div>
+      </div>
+    </div>
     </>
   )
 }
