307a08ecaa19a5184fb59e4664a39361515da281
BkStyle89 <kajzer@hotmail.it>
2026-03-16 12:02:35 +0100

    select option

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+
 [
    { title: 'Inception', genre: 'Fantascienza' },
    { title: 'Il Padrino', genre: 'Thriller' },
@@ -9,16 +10,26 @@ import { useState } from 'react'
    { title: 'Pulp Fiction', genre: 'Thriller' },
  ]
 
+
+
 function App() {
 
-const [films,setFilms] = useState('')
+/* const [films,setFilms] = useState(movies) */
+
 
   return (
     <>
     <div className='container'>
       <div className='row'>
         <div className='col'>
-          
+         <label For="genre">Choose the genre:</label>
+         <select name="filmGenre" id="filmGenre">
+          <option value="Fantascenza">Fantascenza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+         </select>
+        <p>film del genere scelto:{}</p>        
         </div>
       </div>
     </div>
